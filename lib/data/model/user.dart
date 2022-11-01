1584dfb0db1d7ec468b67021b5cbddb4d6a57ef5
PDA2304 <isip_d.a.pahomov@mpt.ru>
2022-11-01 10:33:05 +0300

    .

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -1,3 +1,4 @@
+import 'package:pr2/domain/entity/role_entity.dart';
 import 'package:pr2/domain/entity/user_entity.dart';
 
 class User extends UserEntity {
@@ -15,4 +16,15 @@ class User extends UserEntity {
       'id_role': idRole.id,
     };
   }
+
+  factory User.toFromMap(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as int,
+      login: json['login'],
+      password: json['password'],
+      idRole: RoleEnum.values
+          .firstWhere((element) => element.id == (json['id_role'] as int)),
+    );
+  }
+
 }
--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:pr2/common/data_base_request.dart';
 import 'package:pr2/core/db/data_base_helper.dart';
+import 'package:pr2/data/model/user.dart';
 import 'package:pr2/domain/entity/role_entity.dart';
 import 'package:dartz/dartz.dart';
 import 'package:pr2/domain/repositories/auth_repositories.dart';
@@ -12,15 +13,39 @@ class AuthRepositoryImpl implements AuthRepositories {
   String get table => DataBaseRequest.tableUsers;
 
   @override
-  Future<Either<String, RoleEnum>> signIn(String login, String password) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<Either<String, RoleEnum>> signIn(String login, String password) async {
+    try {
+      var user =
+          (await _db.query(table, where: 'login = ?', whereArgs: [login]))
+              .map((e) => User.toFromMap(e));
+
+      if (user.isEmpty) {
+        return const Left('Такого пользователя нет');
+      }
+
+      if (user.first.password != password) {
+        return const Left('Неправильный пароль');
+      }
+
+      return Right(user.first.idRole);
+    } on DatabaseException catch (error) {
+      return const Left('');
+    }
   }
 
   @override
-  Future<Either<String, bool>> signUp(String login, String password) {
+  Future<Either<String, bool>> signUp(String login, String password) async {
     try {
-      _db.insert(table, Users());
-    } on DatabaseException catch (error) {}
+      await _db.insert(
+          table,
+          User(
+            login: login,
+            password: password,
+            idRole: RoleEnum.user,
+          ).toMap());
+      return const Right(true);
+    } on DatabaseException catch (error) {
+      return const Left('');
+    }
   }
 }
